9936fddbc789d7772f655252f33b00e4decce7e4
Pharteema94 <ibrahimonyeche@gmail.com>
2023-02-22 15:44:42 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+include "main.h"
+/**
+ * main - A program that print _putchar
+ * is written by pharteema
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ins[] = "_putchar";
+int i = 0;
+while (ins[i] != '\0')
+{
+	_putchar(ins[i]);
+	i++;
+}
+	_putchar('\n');
+return (0);
+}
